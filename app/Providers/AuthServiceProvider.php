d2c60a27096acc93ae65742de3e53b9f9354b76f
Lewis <zlwcoder@126.com>
2019-01-13 04:43:09 +0000

    修改和删除收货地址

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,23 +4,16 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use App\Models\UserAddress;
+use App\Policies\UserAddressPolicy;
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * The policy mappings for the application.
-     *
-     * @var array
-     */
     protected $policies = [
         'App\Model' => 'App\Policies\ModelPolicy',
+        UserAddress::class => UserAddressPolicy::class,
     ];
-
-    /**
-     * Register any authentication / authorization services.
-     *
-     * @return void
-     */
+   
     public function boot()
     {
         $this->registerPolicies();
