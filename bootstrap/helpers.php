236b5764103fab231a8e7dbe2da5e8dbbd7c5f80
Lewis <zlwcoder@126.com>
2019-01-13 02:27:37 +0000

    基础布局

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -2,3 +2,8 @@
 function test_helper() {
     return 'OK';
 }
+
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
+}
---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      由 <a href="https://github.com/LewisCoder" target="_blank">LewisZ</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+    </p>
+
+    <p class="float-right"><a href="#">联系我们</a></p>
+  </div>
+</footer>
