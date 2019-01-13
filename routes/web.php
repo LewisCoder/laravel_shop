cf0aadca170ab0f5f537cfce196164a8cc4f6868
Lewis <zlwcoder@126.com>
2019-01-13 12:25:10 +0000

    用户界面-商品收藏

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,13 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')->name('user_addresses.edit');
     Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
     Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
-    Route::get('products', 'ProductsController@index')->name('products.index');
-	Route::get('products/{product}', 'ProductsController@show')->name('products.show');
+    
+	Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
+    Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
 });
+
+Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 
 
 
