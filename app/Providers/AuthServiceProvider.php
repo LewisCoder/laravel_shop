9fa3c2dbbbd2f556a580ea45a2805090074d31ed
Lewis <zlwcoder@126.com>
2019-01-14 02:27:46 +0000

    用户界面-订单详情页

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -6,12 +6,15 @@ use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use App\Models\UserAddress;
 use App\Policies\UserAddressPolicy;
+use App\Models\Order;
+use App\Policies\OrderPolicy;
 
 class AuthServiceProvider extends ServiceProvider
 {
     protected $policies = [
         'App\Model' => 'App\Policies\ModelPolicy',
         UserAddress::class => UserAddressPolicy::class,
+        Order::class       => OrderPolicy::class,
     ];
    
     public function boot()
